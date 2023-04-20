e4d99cefc36b868ee6b59ad2feaf6f4bec9df9dc
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-04-20 14:35:58 +0000

    update and npm install antd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,19 @@
+import React, { useEffect } from "react";
 import HomeComponent from "../components/HomeComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import Loader from "../components/common/Loader";
 const Home = () => {
+
+  useEffect(() => {
+    let navigate = useNavigate();
+    onAuthStateChanged(auth, (res) => {
+      if (!res?.accessToken) {
+        navigate("/");
+      } 
+    });
+  }, []);
   return (
     <HomeComponent />
   )
