71f2187f3a250bbc4342d812783a1a18ea278084
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-02 15:11:59 +0000

    npm install react-icons --save

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,10 +1,26 @@
 import careerLinks from "../../../assets/careerLinks.jpg";
+import { AiOutlineHome, AiOutlineSearch, AiOutlineUserSwitch, AiOutlineMessage, AiOutlineBell } from "react-icons/ai";
+//   AiOutlineHome,
+//   // AiOutlineUserSwitch,
+//   // AiOutlineSearch,
+//   // AiOutlineMessage,
+//   // AiOutlineBell,
+// } from "react-icons/ai";
+
 import './index.scss';
 
 const Topbar = () => {
   return (
     <div className="topbar-main">
       <img className="career-link-logo" src={careerLinks} alt="careerLink logo"/>
+      <div  className="react-icon">
+        <AiOutlineSearch size={25} />
+        <AiOutlineHome size={25}  />
+        <AiOutlineUserSwitch size={25} />
+        <AiOutlineMessage size={25} />
+        <AiOutlineBell size={25} />
+
+      </div>
     </div>
   )
 }
